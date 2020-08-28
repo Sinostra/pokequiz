ac5717e75d93ae03c6a4c615090492f57cb05ca8
Spanjaard <david.spanjaard@externe.bnpparibas.com>
2020-08-28 17:34:13 +0200

    debug 1ère gen

--- src/store/pokedex/data-dex.js
+++ src/store/pokedex/data-dex.js
@@ -19,6 +19,556 @@ export default {
         "type": ["grass", "poison"]
     },
 
+    "3": {
+        "name": {
+            "english": "Venusaur",
+            "french": "Florizarre",
+            "german": "Bisaflor"
+        },
+        "gen": "1",
+        "type": ["grass", "poison"]
+    },
+
+    "4": {
+        "name": {
+            "english": "Charmander",
+            "french": "Salamèche",
+            "german": "Glumanda"
+        },
+        "gen": "1",
+        "type": ["fire"]
+    },
+
+    "5": {
+        "name": {
+            "english": "Charmeleon",
+            "french": "Reptincel",
+            "german": "Glutexo"
+        },
+        "gen": "1",
+        "type": ["fire"]
+    },
+
+    "6": {
+        "name": {
+            "english": "Charizard",
+            "french": "Dracaufeu",
+            "german": "Glurak"
+        },
+        "gen": "1",
+        "type": ["fire"]
+    },
+
+    "7": {
+        "name": {
+            "english": "Squirtle",
+            "french": "Carapuce",
+            "german": "Schiggy"
+        },
+        "gen": "1",
+        "type": ["water"]
+    },
+
+    "8": {
+        "name": {
+            "english": "Wartortle",
+            "french": "Carabaffe",
+            "german": "Schillok"
+        },
+        "gen": "1",
+        "type": ["water"]
+    },
+
+    "9": {
+        "name": {
+            "english": "Blastoise",
+            "french": "Tortank",
+            "german": "Turtok"
+        },
+        "gen": "1",
+        "type": ["water"]
+    },
+
+    "10": {
+        "name": {
+            "english": "Caterpie",
+            "french": "Chenipan",
+            "german": "Raupy"
+        },
+        "gen": "1",
+        "type": ["bug"]
+    },
+
+    "11": {
+        "name": {
+            "english": "Metapod",
+            "french": "Chrysacier",
+            "german": "Safcon"
+        },
+        "gen": "1",
+        "type": ["bug"]
+    },
+
+    "12": {
+        "name": {
+            "english": "Butterfree",
+            "french": "Papilusion",
+            "german": "Smettbo"
+        },
+        "gen": "1",
+        "type": ["bug", "flying"]
+    },
+
+    "13": {
+        "name": {
+            "english": "Weedle",
+            "french": "Aspicot",
+            "german": "Hornliu"
+        },
+        "gen": "1",
+        "type": ["bug", "poison"]
+    },
+
+    "14": {
+        "name": {
+            "english": "Kakuna",
+            "french": "Coconfort",
+            "german": "Kokuna"
+        },
+        "gen": "1",
+        "type": ["bug", "poison"]
+    },
+
+    "15": {
+        "name": {
+            "english": "Beedrill",
+            "french": "Dardargnan",
+            "german": "Bibor"
+        },
+        "gen": "1",
+        "type": ["bug", "poison"]
+    },
+
+    "16": {
+        "name": {
+            "english": "Pidgey",
+            "french": "Roucool",
+            "german": "Taubsi"
+        },
+        "gen": "1",
+        "type": ["normal", "flying"]
+    },
+
+    "17": {
+        "name": {
+            "english": "Pidgeotto",
+            "french": "Roucoups",
+            "german": "Tauboga"
+        },
+        "gen": "1",
+        "type": ["normal", "flying"]
+    },
+
+    "18": {
+        "name": {
+            "english": "Pidgeot",
+            "french": "Roucarnage",
+            "german": "Tauboss"
+        },
+        "gen": "1",
+        "type": ["normal", "flying"]
+    },
+
+    "19": {
+        "name": {
+            "english": "Rattata",
+            "french": "Rattata",
+            "german": "Rattfratz"
+        },
+        "gen": "1",
+        "type": ["normal"]
+    },
+
+    "20": {
+        "name": {
+            "english": "Raticate",
+            "french": "Rattatac",
+            "german": "Rattikarl"
+        },
+        "gen": "1",
+        "type": ["normal"]
+    },
+
+    "21": {
+        "name": {
+            "english": "Spearow",
+            "french": "Piafabec",
+            "german": "Habitak"
+        },
+        "gen": "1",
+        "type": ["normal", "flying"]
+    },
+
+    "22": {
+        "name": {
+            "english": "Fearow",
+            "french": "Rapasdepic",
+            "german": "Ibitak"
+        },
+        "gen": "1",
+        "type": ["normal", "flying"]
+    },
+
+    "23": {
+        "name": {
+            "english": "Ekans",
+            "french": "Abo",
+            "german": "Rettan"
+        },
+        "gen": "1",
+        "type": ["poison"]
+    },
+
+    "24": {
+        "name": {
+            "english": "Arbok",
+            "french": "Arbok",
+            "german": "Arbok"
+        },
+        "gen": "1",
+        "type": ["poison"]
+    },
+
+    "25": {
+        "name": {
+            "english": "Pikachu",
+            "french": "Pikachu",
+            "german": "Pikachu"
+        },
+        "gen": "1",
+        "type": ["electric"]
+    },
+
+    "26": {
+        "name": {
+            "english": "Raichu",
+            "french": "Raichu",
+            "german": "Raichu"
+        },
+        "gen": "1",
+        "type": ["electric"]
+    },
+
+    "27": {
+        "name": {
+            "english": "Sandshrew",
+            "french": "Sabelette",
+            "german": "Sandan"
+        },
+        "gen": "1",
+        "type": ["ground"]
+    },
+
+    "28": {
+        "name": {
+            "english": "Sandslash",
+            "french": "Sablaireau",
+            "german": "Sandamer"
+        },
+        "gen": "1",
+        "type": ["ground"]
+    },
+
+    "29": {
+        "name": {
+            "english": "Nidoran♀",
+            "french": "Nidoran♀",
+            "german": "Nidoran♀"
+        },
+        "gen": "1",
+        "type": ["poison"]
+    },
+
+    "30": {
+        "name": {
+            "english": "Nidorina",
+            "french": "Nidorina",
+            "german": "Nidorina"
+        },
+        "gen": "1",
+        "type": ["poison"]
+    },
+
+    "31": {
+        "name": {
+            "english": "Nidoqueen",
+            "french": "Nidoqueen",
+            "german": "Nidoqueen"
+        },
+        "gen": "1",
+        "type": ["poison", "ground"]
+    },
+
+    "32": {
+        "name": {
+            "english": "Nidoran♂",
+            "french": "Nidoran♂",
+            "german": "Nidoran♂"
+        },
+        "gen": "1",
+        "type": ["poison"]
+    },
+
+    "33": {
+        "name": {
+            "english": "Nidorino",
+            "french": "Nidorino",
+            "german": "Nidorino"
+        },
+        "gen": "1",
+        "type": ["poison"]
+    },
+
+    "34": {
+        "name": {
+            "english": "Nidoking",
+            "french": "Nidoking",
+            "german": "Nidoking"
+        },
+        "gen": "1",
+        "type": ["poison", "ground"]
+    },
+
+    "35": {
+        "name": {
+            "english": "Clefairy",
+            "french": "Mélofée",
+            "german": "Piepi"
+        },
+        "gen": "1",
+        "type": ["fairy"]
+    },
+
+    "36": {
+        "name": {
+            "english": "Clefable",
+            "french": "Mélodelfe",
+            "german": "Pixi"
+        },
+        "gen": "1",
+        "type": ["fairy"]
+    },
+
+    "37": {
+        "name": {
+            "english": "Vulpix",
+            "french": "Goupix",
+            "german": "Vulpix"
+        },
+        "gen": "1",
+        "type": ["fire"]
+    },
+
+    "38": {
+        "name": {
+            "english": "Ninetales",
+            "french": "Feunard",
+            "german": "Vulnona"
+        },
+        "gen": "1",
+        "type": ["fire"]
+    },
+
+    "39": {
+        "name": {
+            "english": "Jigglypuff",
+            "french": "Rondoudou",
+            "german": "Pummeluff"
+        },
+        "gen": "1",
+        "type": ["normal", "fairy"]
+    },
+
+    "40": {
+        "name": {
+            "english": "Wigglytuff",
+            "french": "Grodoudou",
+            "german": "Knuddeluff"
+        },
+        "gen": "1",
+        "type": ["normal", "fairy"]
+    },
+
+    "41": {
+        "name": {
+            "english": "Zubat",
+            "french": "Nosférapti",
+            "german": "Zubat"
+        },
+        "gen": "1",
+        "type": ["poison", "flying"]
+    },
+
+    "42": {
+        "name": {
+            "english": "Golbat",
+            "french": "Nosféralto",
+            "german": "Golbat"
+        },
+        "gen": "1",
+        "type": ["poison", "flying"]
+    },
+
+    "43": {
+        "name": {
+            "english": "Oddish",
+            "french": "Mystherbe",
+            "german": "Myrapla"
+        },
+        "gen": "1",
+        "type": ["grass", "poison"]
+    },
+
+    "44": {
+        "name": {
+            "english": "Gloom",
+            "french": "Ortide",
+            "german": "Duflor"
+        },
+        "gen": "1",
+        "type": ["grass", "poison"]
+    },
+
+    "45": {
+        "name": {
+            "english": "Vileplume",
+            "french": "Rafflésia",
+            "german": "Giflor"
+        },
+        "gen": "1",
+        "type": ["grass", "poison"]
+    },
+
+    "46": {
+        "name": {
+            "english": "Paras",
+            "french": "Paras",
+            "german": "Paras"
+        },
+        "gen": "1",
+        "type": ["bug", "grass"]
+    },
+
+    "47": {
+        "name": {
+            "english": "Parasect",
+            "french": "Parasect",
+            "german": "Parasek"
+        },
+        "gen": "1",
+        "type": ["bug", "grass"]
+    },
+
+    "48": {
+        "name": {
+            "english": "Venonat",
+            "french": "Mimitoss",
+            "german": "Bluzuk"
+        },
+        "gen": "1",
+        "type": ["bug", "poison"]
+    },
+
+    "49": {
+        "name": {
+            "english": "Venomoth",
+            "french": "Aéromite",
+            "german": "Omot"
+        },
+        "gen": "1",
+        "type": ["bug", "poison"]
+    },
+
+    "50": {
+        "name": {
+            "english": "Diglett",
+            "french": "Taupiqueur",
+            "german": "Digda"
+        },
+        "gen": "1",
+        "type": ["ground"]
+    },
+
+    "51": {
+        "name": {
+            "english": "Dugtrio",
+            "french": "Triopikeur",
+            "german": "Digdri"
+        },
+        "gen": "1",
+        "type": ["ground"]
+    },
+
+    "52": {
+        "name": {
+            "english": "Meowth",
+            "french": "Miaouss",
+            "german": "Mauzi"
+        },
+        "gen": "1",
+        "type": ["normal"]
+    },
+
+    "53": {
+        "name": {
+            "english": "Persian",
+            "french": "Persian",
+            "german": "Snobilikat"
+        },
+        "gen": "1",
+        "type": ["normal"]
+    },
+
+    "54": {
+        "name": {
+            "english": "Psyduck",
+            "french": "Psykokwak",
+            "german": "Enton"
+        },
+        "gen": "1",
+        "type": ["water"]
+    },
+
+    "55": {
+        "name": {
+            "english": "Golduck",
+            "french": "Akwakwak",
+            "german": "Entoron"
+        },
+        "gen": "1",
+        "type": ["water"]
+    },
+
+    "56": {
+        "name": {
+            "english": "Mankey",
+            "french": "Férosinge",
+            "german": "Menki"
+        },
+        "gen": "1",
+        "type": ["fighting"]
+    },
+
+    "57": {
+        "name": {
+            "english": "Primeape",
+            "french": "Colossinge",
+            "german": "Rasaff"
+        },
+        "gen": "1",
+        "type": ["fighting"]
+    },
+
     "155": {
         "name": {
             "english": "Cyndaquil",
