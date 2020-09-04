e4a3e514634acef90bc03ca83751d512a8488421
Spanjaard <david.spanjaard@externe.bnpparibas.com>
2020-09-04 19:00:24 +0200

    mise en place composant timer + vue config js

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="game">
-
+        
         <div v-if="gameReady" class="interface-wrapper">
 
             <div class="gameInfos">
@@ -15,10 +15,7 @@
                     
                 </div>
 
-                <div class="timer-wrapper">
-                    <div>{{$store.state.localisation.dataLang['remainingTimeText']}}</div>
-                    <div>{{displayMinsAndSecs(totalTime)}}</div>
-                </div>
+                <Timer v-on:time-up="finishGame()" :gameState="gameState"/>
 
                 <div class="timer-btn-wrapper">
                     <div v-if="gameState == 'playing'" v-on:click="pauseGame()" class="pauseBtn"></div>
@@ -34,7 +31,7 @@
 
                 <div v-if="gameState == 'playing'" class="input-wrapper">
                     <div class="enterNamesInstruct">{{$store.state.localisation.dataLang['enterNameInstruct']}}</div>
-                    <input v-model="enteredName" v-on:keyup="checkEnteredPokemon" type="text">
+                    <input v-model="enteredName" v-on:input="checkEnteredPokemon" type="text">
                 </div>
 
                 <div v-if="gameState == 'playing' || gameState == 'paused'" class="btn-wrapper middleBtn">
@@ -78,10 +75,16 @@
         </div>
     </div>
 </template>
-
 <script>
+
+import Timer from './Timer.vue'
+
 export default {
     name: 'Game',
+
+    components: {
+        Timer,
+    },
 
     data: function(){
         return {
@@ -92,8 +95,6 @@ export default {
             displayDex: [],
             enteredName: '',
             score: 0,
-            totalTime: 0,
-            interval: undefined
         }
     },
 
@@ -181,44 +182,16 @@ export default {
             }
         },
 
-        displayMinsAndSecs(secs){
-            var remainSecs
-            var remainMinutes
-
-            if(secs > 59) {
-                remainSecs = secs % 60
-                remainMinutes = (secs - remainSecs)/60
-            }
-
-            else {
-                remainSecs = secs
-                remainMinutes = 0
-            }
-
-            if(remainSecs < 10) remainSecs = "0"+ remainSecs
-            if(remainMinutes < 10) remainMinutes = "0"+ remainMinutes
-
-            return remainMinutes + ':' + remainSecs
-        },
-
         startGame(){
-            this.interval = setInterval(() => {
-                this.totalTime--
-                if(this.totalTime == 0) {
-                    this.finishGame()
-                }
-            }, 1000);
             this.gameState = "playing"
         },
 
         finishGame(){
-            clearInterval(this.interval)
             this.gameState = "finished"
             this.fillMisssing()
         },
 
         pauseGame(){
-            clearInterval(this.interval)
             this.gameState = "paused"
         },
 
@@ -240,8 +213,6 @@ export default {
 
         else if(this.numberOfPokemons >= 200) this.splitNumber = 4
 
-        this.totalTime = this.$store.state.settings.secondsPerPokemon[this.$store.state.settings.selectedDifficulty] * this.numberOfPokemons
-
         for(const index in this.$store.state.pokedex.currentDex) {
             this.$store.state.pokedex.currentDex[index]['found'] = false
             this.$store.state.pokedex.currentDex[index]['forgotten'] = false
@@ -253,6 +224,7 @@ export default {
             this.gameReady = true
         }, 2000)
 
-    }
+    },
+
 }
 </script>
---
+++ src/components/Timer.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="timer">
+        <div>{{$store.state.localisation.dataLang['remainingTimeText']}}</div>
+        <div>{{displayMinsAndSecs(totalTime)}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Timer',
+
+    data: function() {
+        return {
+            numberOfPokemons: 0,
+            totalTime: 0,
+            interval: undefined,
+        }
+    },
+
+    props: ['gameState'],
+
+    watch: {
+        gameState: function(newVal, oldVal) {
+            console.log('Prop changed: ', newVal, ' | was: ', oldVal)
+            if(newVal == "playing") {
+                this.startTimer();
+            }
+
+            else {
+                this.pauseTimer();
+            }
+        }
+    },
+
+    methods: {
+        startTimer(){
+            this.interval = setInterval(() => {
+                this.totalTime--
+                if(this.totalTime == 0) {
+                    this.$emit('time-up')
+                    console.log('time is up')
+                }
+            }, 1000);
+        },
+
+        pauseTimer(){
+            clearInterval(this.interval)
+        },
+
+        displayMinsAndSecs(secs){
+            var remainSecs
+            var remainMinutes
+
+            if(secs > 59) {
+                remainSecs = secs % 60
+                remainMinutes = (secs - remainSecs)/60
+            }
+
+            else {
+                remainSecs = secs
+                remainMinutes = 0
+            }
+
+            if(remainSecs < 10) remainSecs = "0"+ remainSecs
+            if(remainMinutes < 10) remainMinutes = "0"+ remainMinutes
+
+            return remainMinutes + ':' + remainSecs
+        },
+    },
+
+    mounted: function(){
+        this.numberOfPokemons = Object.keys(this.$store.state.pokedex.currentDex).length
+        this.totalTime = this.$store.state.settings.secondsPerPokemon[this.$store.state.settings.selectedDifficulty] * this.numberOfPokemons
+    }
+}
+</script>
