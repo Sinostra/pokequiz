1bcb08a20609a0abc622199a776027614e38b087
Spanjaard <david.spanjaard@externe.bnpparibas.com>
2020-10-06 20:40:19 +0200

    mise en place d'un fichier php pour le back

---
+++ src/components/Score.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="score">
+
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Score',
+
+  methods: {
+
+    getScore(params) {
+        fetch('http://localhost:8081/')
+        .then(response => response.json())
+        .then(data => console.log(data));
+    }
+  }
+}
+</script>
