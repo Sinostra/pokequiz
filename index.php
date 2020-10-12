df80ca2aceccdb6d9791a83719b12f10558a5e12
Spanjaard <david.spanjaard@externe.bnpparibas.com>
2020-10-12 22:24:31 +0200

    fin de mise en place score back + avancement mise en place score front

--- index.php
+++ index.php
@@ -6,10 +6,11 @@ header('Access-Control-Allow-Methods: POST, GET');
 
 //$pdo = new PDO("sqlite:pokequiz.db");
 
-$data = ["error" => "404"];
+$data = [];
 //adminer link : http://localhost:8000/test/adminer-sqlite.php?sqlite=&username=&db=pokequiz.db
 // https://github.com/FrancoisCapon/LoginToASqlite3DatabaseWithoutCredentialsWithAdminer
 if (!extension_loaded('sqlite3')) exit('Module sqlite3 not loaded');
+
 class MyDB extends SQLite3 {
     function __construct() {
         $this->open('pokequiz.db');
@@ -17,39 +18,93 @@ class MyDB extends SQLite3 {
     /**
      * 
      */
-    function insertScore($data) {
-        // var_dump($this->lastInsertRowID());
+    function insertScore($name, $settings, $score, $time) {
+
+        $data = [
+            "error" => false,
+        ];
+
+        $statement = $this->prepare('
+            INSERT INTO "scores" (name, settings, score, time) VALUES (:name, :settings, :score, :time)
+        ');
+
+        $statement->bindValue(':name', $name);
+        $statement->bindValue(':settings', $settings);
+        $statement->bindValue(':score', $score);
+        $statement->bindValue(':time', $time);
+
+        $result = $statement->execute();
+
+        if($result) {
+
+            $data = [
+                "id" => $this->lastInsertRowID()
+            ];
+        }
+
+        else $data['error'] = true;
+
+        return $data;
     }
     /**
      * 
      */
     function listScore($settings) {
-        $sql = '
-            SELECT * FROM "scores" WHERE "settings" = "'
-        .$settings.'"';
 
-        $data = [];
+        $data = [
+            "error" => false,
+            "empty" => true
+        ];
+
+        $sql = '
+            SELECT * FROM "scores" WHERE "settings" = "'.$settings.'" ORDER BY "score" DESC, "time" ASC
+        ';
+
 
         $response = $this->query($sql);
-        $result = $response->fetchArray(SQLITE3_ASSOC);
-        $data[] = $result;
 
-        if(empty($data)) {
-            $data = 'empty';
+        if(!$response) {
+            $data['error'] = true;
+        }
+
+        else {
+
+            while($result = $response->fetchArray(SQLITE3_ASSOC)) {
+                $data['result'][] = $result;
+                $data['empty'] = false;
+            }
         }
 
         return $data;
+
     }
 }
+
+$route = 'select';
+if(isset($_GET['route'])) {
+    $route = $_GET['route'];
+}
+
+$params = json_decode($_POST['payload'], true);
 $db = new MyDB();
+
+
 if(!$db) {
-    //echo $db->lastErrorMsg();
+    $data['error'] = true;
 } else {
-    //echo "Opened database successfully\n";
-    $data = $db->listScore(json_decode($_POST['payload'], true)['settings']);
+
+    switch($route) {
+        case 'select':
+            $data = $db->listScore($params['settings']);
+        break;
+
+        case 'add':
+            $data = $db->insertScore($params['name'], $params['settings'], $params['score'], $params['time']);
+        break;
+    }
+
+    
     $db->close();
 }
 
-
-// var_dump(json_decode($_POST['payload'], true)['settings']);
 echo json_encode($data);
--- src/components/Score.vue
+++ src/components/Score.vue
@@ -1,50 +1,140 @@
 <template>
   <div class="score">
-    <div>Mon score affiché ici</div>
-    <div v-if="hasError">Une erreur est survenue, veuillez réessayer plus tard.</div>
+    <div class="score-wrapper">
+      <div v-if="!hasError && !canDisplayScores" class="input-wrapper">
+        <label for="name">Veuillez renseigner votre nom</label>
+        <input v-model="playerName" id="name" type="text" />
+        <div class="btn-wrapper">
+          <div v-on:click="addScore()" class="btn submit">Valider</div>
+        </div>
+      </div>
+      <div v-if="!hasError && canDisplayScores" class="table-scores-wrapper">
+
+        <div class="settings">
+          <div class="chosenGens">
+            <div class="label">Générations choisies :</div>
+            <div class="gens-wrapper">
+              <div class="gen" v-for="(gen, cgen) in $store.state.settings.selectedGeneration" :key="cgen">
+                {{gen}}
+              </div>
+            </div>
+          </div>
+          
+          <div class="chosenTypes">
+            <div class="label">Types choisis :</div>
+            <div class="types-wrapper">
+              <div class="type" v-for="(type, ctype) in $store.state.settings.selectedTypes" :key="ctype">
+                <img :src="getUrl(type)">
+              </div>
+            </div>
+          </div>
+
+          <div class="chosenDifficulty">
+            <div class="label">Difficulté choisie :</div>
+            {{$store.state.localisation.dataLang[this.$store.state.settings.selectedDifficulty + 'Text']}}
+          </div>
+        </div>
+
+        <div class="emptyScores" v-if="emptyScores">Pas de score à afficher</div>
+
+        <div v-if="!emptyScores" class="table-scores">
+          <div class="table-head">
+            <div class="table-cell">Name</div>
+            <div class="table-cell">Score</div>
+            <div class="table-cell">Time</div>
+          </div>
+          <div
+            v-for="(score, index) in allScores['result']"
+            :key="index"
+            class="table-row"
+            :class="score.id == lastEnteredId ? 'playedGame' : ''"
+          >
+            <div class="table-cell">{{ score.name }}</div>
+            <div class="table-cell">{{ score.score }}/{{ totalNumber }}</div>
+            <div class="table-cell">{{ displayMinsAndSecs(score.time) }}</div>
+          </div>
+        </div>
+      </div>
+      <div v-if="hasError">
+        Une erreur est survenue, veuillez réessayer plus tard.
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Score",
 
-  data: function(){
+  data: function () {
     return {
-      playerName: "Sino",
-      gameSettings: this.$store.state.settings.selectedGeneration + " " + this.$store.state.settings.selectedTypes + " " + this.$store.state.settings.selectedDifficulty,
+      playerName: "",
+      gameSettings:
+        this.$store.state.settings.selectedGeneration +
+        " " +
+        this.$store.state.settings.selectedTypes +
+        " " +
+        this.$store.state.settings.selectedDifficulty,
       numberOfFound: 0,
       totalNumber: Object.keys(this.$store.state.pokedex.currentDex).length,
       timeUsed: this.$store.state.settings.timeUsed,
-      hasError: false
-    }
+      hasError: false,
+      canDisplayScores: false,
+      allScores: {},
+      lastEnteredId: 0,
+      emptyScores: false
+    };
   },
 
   methods: {
     getScore() {
-
       let payload = {
-        hasBeenPlayed: this.$store.state.settings.hasBeenPlayed,
-        settings: this.gameSettings
-      }
-
-      if(this.$store.state.settings.hasBeenPlayed) {
-        payload.name = this.playerName,
-        payload.score = this.numberOfFound,
-        payload.time = this.timeUsed
-      }
+        settings: this.gameSettings,
+      };
 
       var formData = new FormData();
-      formData.append('payload', JSON.stringify(payload))
+      formData.append("payload", JSON.stringify(payload));
 
       fetch("http://localhost:8081/", {
-        method: 'POST',
+        method: "POST",
         body: formData,
       })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch(() => this.hasError = true)
+        .then((response) => response.json())
+        .then((data) => {
+          this.hasError = data.hasError;
+          this.emptyScores = data.empty;
+          this.allScores = data;
+          this.canDisplayScores = true;
+        })
+        .catch(() => (this.hasError = true));
+    },
+
+    addScore() {
+      for(const index in this.$store.state.pokedex.currentDex){
+        if(this.$store.state.pokedex.currentDex[index]['found']) this.numberOfFound++
+      }
+
+      let payload = {
+        settings: this.gameSettings,
+        name: this.playerName,
+        score: this.numberOfFound,
+        time: this.timeUsed,
+      };
+
+      var formData = new FormData();
+      formData.append("payload", JSON.stringify(payload));
+
+      fetch("http://localhost:8081?route=add", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          this.hasError = data.hasError;
+          this.lastEnteredId = data.id;
+          this.getScore()
+        })
+        .catch(() => (this.hasError = true));
     },
 
     playAgain() {
@@ -52,34 +142,52 @@ export default {
       this.$store.dispatch("rePlay");
     },
 
+    displayMinsAndSecs(secs) {
+      var remainSecs;
+      var remainMinutes;
+
+      if (secs > 59) {
+        remainSecs = secs % 60;
+        remainMinutes = (secs - remainSecs) / 60;
+      } else {
+        remainSecs = secs;
+        remainMinutes = 0;
+      }
+
+      if (remainSecs < 10) remainSecs = "0" + remainSecs;
+      if (remainMinutes < 10) remainMinutes = "0" + remainMinutes;
+
+      return remainMinutes + ":" + remainSecs;
+    },
+
+    getUrl(type) {
+      return require('../assets/img/languages/' + this.$store.state.localisation.chosenLang + '/types/'+ type + '.png')
+    },
   },
 
-  mounted: function(){
-
-    console.clear()
-
-    if(this.$store.state.settings.hasBeenPlayed) {
-
-      for(const index in this.$store.state.pokedex.currentDex){
-        if(this.$store.state.pokedex.currentDex[index]['found']) this.numberOfFound++
-      }
-
-      this.getScore()
+  mounted: function () {
+    if (!this.$store.state.settings.hasBeenPlayed) {
+      this.getScore();
     }
 
-    else {
+    // if(this.$store.state.settings.hasBeenPlayed) {
 
-      try {
-        this.getScore()
-      }
-  
-      catch(error) {
-        console.log(error)
-        this.hasError = true;
-      }
-    }
-    
-  }
+    //   for(const index in this.$store.state.pokedex.currentDex){
+    //     if(this.$store.state.pokedex.currentDex[index]['found']) this.numberOfFound++
+    //   }
+    // }
 
+    // else {
+
+    //   try {
+    //     this.getScore()
+    //   }
+
+    //   catch(error) {
+    //     console.log(error)
+    //     this.hasError = true;
+    //   }
+    // }
+  },
 };
 </script>
